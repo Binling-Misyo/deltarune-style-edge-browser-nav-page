a6e7bed3cadb7c9122b47087bd3926d53e6fab73
Binling-Misyo <2862177018@qq.com>
2026-04-13 00:04:34 +0800

    存储基础更新
    1.开始搭建用户自定义导航项功能，目前已经完成读、写、解析三步已经完成了读和解析
    2.删除了冗余内容
    3.控制页面改名为设置，以后会在这里新增自定义导航项
    4.添加了抖音的图标
    5.修复了悬停在导航项上文字不高亮显示的bug

--- src/main.js
+++ src/main.js
@@ -1,3 +1,47 @@
+class LinkDiv{
+    constructor(name,url,id,imgPath){
+        this.name = name;
+        this.url = url;
+        this.id = id;
+        this.imgPath = imgPath;
+        console.log("新增的链接：", this.name, this.url, this.id, this.imgPath);
+    }
+    printHTML(){
+        console.log("打印链接：", this.name, this.url, this.id, this.imgPath);
+        return `<a href="${this.url}">
+            <div>
+                <img src="${this.imgPath}" alt=""><br> <span lang="en">*${this.name}</span> 
+            </div>
+        </a>`;
+    }
+}
+const DefaultLinkList = [
+  new LinkDiv("超星学习通", "https://i.chaoxing.com/base?t=1749358614086", 0, "../image/xxt.jpg"),
+  new LinkDiv("哔哩哔哩", "https://www.bilibili.com/", 1, "../image/bilibili.png"),
+  new LinkDiv("深度求索", "https://chat.deepseek.com/sign_in", 2, "../image/ds.png"),
+  new LinkDiv("周师图书馆", "http://tsg.zknu.edu.cn:8080/reader/login.php", 3, "../image/zknuli.png"),
+  new LinkDiv("洛谷", "https://www.luogu.com.cn/", 4, "../image/luogu.png"),
+  new LinkDiv("Github", "https://github.com/", 5, "../image/git.png"),
+  new LinkDiv("MikuTools", "https://tools.miku.ac/", 6, "../image/绘制网站 Logo.png"),
+  new LinkDiv("ChatGPT", "https://chatgpt.com/", 7, "../image/chatgpt.png"),
+  new LinkDiv("抖音", "https://www.douyin.com/", 8, "../image/douyin.png"),
+  new LinkDiv("鸽了", "javascript:void(0)", 9, "../image/gugu.png")
+];
+const loadLinkList = () => {
+  try {
+    const list = JSON.parse(localStorage.getItem('linkList')) || DefaultLinkList;
+    return list.map(i => new LinkDiv(i.name, i.url, i.id, i.imgPath));
+  } catch {
+    return DefaultLinkList.map(i => new LinkDiv(i.name, i.url, i.id, i.imgPath));
+  }
+};
+
+function updateLinkList(linkList,container){
+    container.innerHTML = linkList.map(i => i.printHTML()).join('');
+}
+let linkList = loadLinkList();
+updateLinkList(linkList,document.querySelector(".ptr"));
+
 const searchForm = document.getElementById("searchForm");
 const searchEngine = document.getElementById("searchEngine");
 const searchInput = document.getElementById("searchInput");
